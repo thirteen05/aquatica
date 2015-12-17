c775f6c2896f1befccaed819511285c3fe0bbcc3
Jed Darrohn <jed@thirteen05.com>
2015-12-17 16:17:03 -0500

    Created Slider for Floor Plans on Home Page

--- sections/home/section-home-plans.php
+++ sections/home/section-home-plans.php
@@ -12,22 +12,49 @@
         <div class="center-wrap">
           <img src="<?php echo get_template_directory_uri(); ?>/images/home/Stunning.svg"/>
           <hr class="hidden-xs hidden-sm">
-          <ul id="floor-plan-list" class="hidden-xs hidden-sm">
-            <li class="floor-plan font-light uppercase active">Apartment 1</li>
-            <li class="floor-plan font-light uppercase">Apartment 2</li>
-            <li class="floor-plan font-light uppercase">Apartment 3</li>
-            <li class="floor-plan font-light uppercase">Penthouse 1</li>
-            <li class="floor-plan font-light uppercase">Penthouse 2</li>
-          </ul>
           <p>
-            Lorem ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.
+            Lorem ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius.
           </p>
+          
+          <ul id="floor-plan-list" class="hidden-xs hidden-sm nav nav-tabs" role="tablist">
+            <li role="presentation" class="floor-plan font-light uppercase active"><a href="#tab-apartment-1" aria-controls="tab-apartment-1" role="tab" data-toggle="tab">Apartment 1</a></li>
+            <li role="presentation" class="floor-plan font-light uppercase"><a href="#tab-apartment-2" aria-controls="tab-apartment-2" role="tab" data-toggle="tab">Apartment 2</a></li>
+            <li role="presentation" class="floor-plan font-light uppercase"><a href="#tab-apartment-3" aria-controls="tab-apartment-3" role="tab" data-toggle="tab">Apartment 3</a></li>
+            <li role="presentation" class="floor-plan font-light uppercase"><a href="#tab-penthouse-1" aria-controls="tab-penthouse-1" role="tab" data-toggle="tab">Penthouse 1</a></li>
+            <li role="presentation" class="floor-plan font-light uppercase"><a href="#tab-penthouse-2" aria-controls="tab-penthouse-2" role="tab" data-toggle="tab">Penthouse 2</a></li>
+          </ul>
+          
           <button class="btn btn-default btn-aquatica">Contact Us Today to Schedule a Tour</button>
         </div>
       </div>
       <div class="col-md-5 col-md-push-2 hidden-xs hidden-sm">
+        <!-- Tab panes -->
         <div class="center-wrap">
-          <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/home/Floorplan-1.png">
+          <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="tab-apartment-1">
+              <p class="font-white">Lorem1 ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.</p>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="tab-apartment-2">
+              <p class="font-white">
+                 Lorem2 ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="tab-apartment-3">
+              <p class="font-white">
+                Lorem3 ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="tab-penthouse-1">
+              <p class="font-white">
+              Lorem4 ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="tab-penthouse-2">
+              <p class="font-white">
+                Lorem5 ipsum dolor sit amet, vim te suas democritum necessitatibus, graecis habemus reprehendunt et has, senserit efficiantur vel ne. Te cum possit vulputate. At clita constituam eam, te eligendi quaerendum ius. Modo elitr denique eu quo, vis eu partiendo adolescens repudiandae. Nec quidam blandit legendos in, in vix blandit commune, splendide scribentur interpretaris id usu. No munere vituperata pro.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
